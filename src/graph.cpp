da97c9a7e880c9eae4beff8d43c4c320acb1e332
Emi Weel <lode_weel@hotmail.com>
2022-01-13 14:20:00 +0100

    added comments in graph.src

--- src/graph.cpp
+++ src/graph.cpp
@@ -21,9 +21,14 @@ namespace galplib {
         nodeCount = nodes;
         edges = std::vector<std::unordered_map<unsigned int, int>>();
         unsigned int iter = 0;
+        // Note our graph nodes are 1-indexed. This means we need an empty edge set for node 0 to fill the datastructe.
+        // However since index 0 should not be in the input, starting the loop at 0 will take care of that.
         for (int index = 0; index <= nodes; index++){
+            // We make use of the fact that the input is sorted in the left vertex to be able to enforce that this does
+            // not grow after we finish building it for this vertex to avoid segmentation faults.
             std::unordered_map<unsigned int, int> edgeMap = std::unordered_map<unsigned int, int>();
             while (iter < input.size() && input[iter][0] == index){
+                // Add the next line of input to our graph by adding the edge.
                 edgeMap.insert({input[iter][1], input[iter][2]});
                 iter++;
             }
@@ -31,8 +36,10 @@ namespace galplib {
         }
     }
 
+    // This function returns the weight of an edge, or -1 if it's not included in the graph. This assumes that
+    // weights are >= 0.
     int Graph::getEdge(unsigned int i, unsigned int j) {
-        if (i > nodeCount || j >> nodeCount){
+        if (i > nodeCount || j > nodeCount){
             throw std::invalid_argument("node was not in graph!");
         }
         if (i < j){
@@ -53,18 +60,21 @@ namespace galplib {
 
     std::vector<unsigned int> Graph::computePath(const std::vector<unsigned int> & prefs){
         std::unordered_set<unsigned int> visited = std::unordered_set<unsigned int>(prefs.size());
-        unsigned int current = prefs[0];
+        unsigned int current = prefs[0]; // Our nodes are all 1-indexed, meaning the 0th index is free to put as a start node.
         std::vector<unsigned int> path{0};
         while (true) {
+            // We build the path starting at node, then going to prefs[node], e.g. hopping along the preferred edge at
+            // each stage. This is valid so long that node has not been visited yet, hence the visited check.
             visited.insert(current);
             path.push_back(current);
             int w = getEdge(current, prefs[current]);
             if (w >= 0 && !visited.contains(prefs[current])){
                 path[0] +=w;
                 current = prefs[current];
-                continue;
+                continue;   // If we successfully found a new node to add to the path (the if statement), do so
+                            // and iterate the loop, which is what continue does.
             }
-            break;
+            break; // the default behaviour is to break, which feels slightly safer, ending the while loop.
         }
         return path;
     }
